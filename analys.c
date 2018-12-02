85b7aa956cf3cb315556f1d2a23b531387580ffe
jamali005 <37799381+jamali005@users.noreply.github.com>
2018-12-02 22:07:08 +0100

    Add files via upload

---
+++ analys.c
@@ -0,0 +1,265 @@
+#include "analys.h"
+
+#include<stdlib.h>
+
+#include<stdio.h>
+
+extern int yylex();
+
+typetoken token;
+boolean follow_token;
+
+typetoken _lire_token(){
+	if (follow_token){
+		follow_token = false;
+		return token;
+	}
+	else{
+		return (typetoken) yylex();
+	}
+}
+
+
+
+
+
+int main(int argc , char** argv ){
+
+	token = _lire_token();
+	
+	
+	if (_prog() == true){
+		
+		printf(" successful operation\n");
+		
+	}
+	else {
+		
+		printf(" operation failed\n");
+	}
+	
+	
+	/*
+	if (_decl()==true){
+		printf("successful operation\n");
+	}
+	else {
+		printf("operation failed\n");
+	}
+	*/
+	
+}
+
+
+
+
+
+boolean _type(){
+	
+	boolean result;
+	
+	if (token == IDF){
+		result = true;
+	}
+	
+	else if (token == INTEGER){
+		result = true;
+	}
+	
+	else if (token == STRING){
+		result = true;
+	}
+	
+	else if (token == FLOAT){
+		result = true;
+	}
+	
+	else if (token == CHAR){
+		result = true;
+	}
+	
+	else if (token == BOOL){
+		result = true;
+	}
+	
+	else if (token == NATURAL){
+		result = true;
+	}
+	
+	else if (token == POSITIVE){
+		result = true;
+	}
+	
+	else {
+		result = false;
+	}
+	
+	return result;
+	
+}
+
+
+
+boolean _const(){
+	
+	boolean result;
+	
+	if(token == DNUMBER){
+		
+		result=true;
+	}
+	
+	else if(token == INUMBER){
+		
+		result=true;
+	}
+	
+	else if(token == TRUE){
+		
+		result=true;
+	}
+	
+	else if(token == FALSE){
+		
+		result=true;
+	}
+	
+	else {
+		result = false;
+	}
+	
+	return result;
+	
+}
+
+
+
+boolean _decl_aux(){
+	boolean result;
+	
+	if (token == AFFECTATION){
+		printf(" affectation(:=) detected \n");
+		token = _lire_token();
+		
+		if (_const()==true){
+			printf(" const detected\n");
+			result = true;
+			token = _lire_token();
+			_decl_aux();
+		
+		}
+		else {
+			printf(" const NOT detected\n");
+			result = false;
+			}
+	
+	}
+	else if (token==INST){
+		printf(" fin_inst detected\n");
+		result = true;
+	}
+	else{
+		printf(" erreur_decl_aux detected\n");
+		result = false;
+	}
+	
+	return result;
+}
+
+
+
+
+
+boolean _decl(){
+	
+	boolean result = false ;
+	
+	if (token == IDF){
+		printf(" IDF detected \n");
+		token = _lire_token();
+		
+		if (token == GTYPE){
+			printf(" GTYPE(:) detected \n");
+			token = _lire_token();
+			
+			if (_type() == true){
+				printf(" type detected \n");
+				token = _lire_token();
+				
+				if (_decl_aux() == true){
+					result = true;
+				}
+				
+			}else {printf(" type NOT detected \n");}
+				
+		}else {printf(" GTYPE(:) NOT detected \n");}
+		
+	}else {printf(" IDF NOT detected \n");}
+		
+	return result;	
+	
+}
+
+
+
+boolean _liste_decl(){
+	boolean result;
+	if (_decl()){
+		printf("___________________________________\n");
+		token = _lire_token();
+		if (_liste_decl_aux()){
+			result = true;
+		}
+		else {
+			result = false;
+		}
+	}
+	else {
+		result = false;
+	}
+	
+	return result;
+}
+
+
+
+boolean _liste_decl_aux(){
+	boolean result;
+	
+	if (token==BEG_IN){
+		follow_token = true;
+		result = true;
+	}
+	else if (_liste_decl()){
+		result = true;
+	}
+	else {
+		result = false;
+	}
+	
+	return result;
+	
+}
+
+
+
+boolean _prog(){
+	boolean result;
+	
+	if (_liste_decl()){
+		token = _lire_token();
+		if (token == BEG_IN){
+			result = true;
+		}
+		else {
+			result = false;
+		}
+	}
+	else {
+		result = false;
+	}
+	
+	return result;
+}
+
+
---
+++ analys.h
@@ -0,0 +1,53 @@
+#ifndef ANALYS_H
+
+#define ANALYS_H
+
+
+
+typedef enum{
+	INST,
+	COMMENTARY,
+	PACKAGE,
+	WITH,
+	USE,
+	PROCEDURE,
+	IS,
+	BEG_IN,
+	END,
+	INTEGER,
+	STRING,
+	FLOAT,
+	CHAR,
+	BOOL,
+	NATURAL,
+	POSITIVE,
+	TRUE,
+	FALSE,
+	IDF,
+	AFFECTATION,
+	GTYPE,
+	DNUMBER,
+	INUMBER
+
+}typetoken;
+
+
+
+typedef enum{
+
+false=0, true=1}boolean;
+
+
+
+
+typetoken _lire_token();
+boolean _type();
+boolean _const();
+boolean _decl_aux();
+boolean _decl();
+boolean _liste_decl();
+boolean _liste_decl_aux();
+boolean _prog();
+
+
+#endif
