034d0bc9f149bd3272b825aeb9da7245e7060110
jamali005 <37799381+jamali005@users.noreply.github.com>
2019-02-02 14:47:09 +0100

    compilation project for ada spark
    I was inspired by project of my professor K.baina 
    https://gitlab.com/kbaina/hortensias/tree/master/hortensias_2.1

--- analys.h
+++ analys.h
@@ -1,11 +1,11 @@
 #ifndef ANALYS_H
 
 #define ANALYS_H
-
+#include"cfg.h"
 
 
 typedef enum{
-	INST,
+	
 	COMMENTARY,
 	PACKAGE,
 	WITH,
@@ -27,17 +27,38 @@ typedef enum{
 	AFFECTATION,
 	GTYPE,
 	DNUMBER,
-	INUMBER
+	INUMBER,
+	POPEN,
+	PCLOSE,
+	VIRG,
+	CONSTANT,
+	PVIRG,
+	LOOPFOR,
+	IN,
+	BETWEEN,
+	LOOP,
+	PUTLINE,
+	IF,
+	ELSE,
+	THEN,
+	//ELSEIF
+	EGAL,
+	TIRETS,
+	PLUS,
+	MINUS,
+	MULT,
+	DIV,CARA,
+	ERRL
 
 }typetoken;
 
-
+/*
 
 typedef enum{
 
 false=0, true=1}boolean;
 
-
+*/
 
 
 typetoken _lire_token();
@@ -48,6 +69,34 @@ boolean _decl();
 boolean _liste_decl();
 boolean _liste_decl_aux();
 boolean _prog();
+boolean _constaux();
+boolean _array();
+boolean _listenum();
+boolean _listnum_aux();
+boolean _liste_inst();
+boolean _liste_inst_aux();
+boolean _instr();
+boolean _condition();
+boolean _instement();
+boolean _else_instement();
+boolean _else_statement();
+boolean oper();
+boolean _inst();
+boolean if_insteaux();
+boolean inst();
+boolean _liste_inst();
+boolean _addsub();
+boolean _multdivaux();
+boolean _multdiv();
+boolean _aux();
+boolean _addsubaux();
+boolean _affectation();
+
+void set_idf_attributes(char *name, int line);
+void set_number_attributes(double value);
+void set_string_attributes(char * s);
+void set_token_attributes(int line);
+
 
 
 #endif
